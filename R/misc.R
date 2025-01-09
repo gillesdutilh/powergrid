a38ac5822108f61b703e1b19131667aaeb3f0419
Gilles Dutilh <gilles.dutilh@gmail.com>
2025-01-09 18:48:59 +0100

    weekend

--- R/misc.R
+++ R/misc.R
@@ -6,4 +6,26 @@
 ##' @return a vector with the desired minimum and maximum
 ##' @author Gilles Dutilh
 ScaleRange = function(x, a, b){
-  (b - a) * (x - min(x)) / (max(x) - min(x)) + a}
+  (b - a) * (x - min(x)) / (max(x) - min(x)) + a
+}
+
+##' @title Wrap text in printed output.
+##' @description Wrap text in printed output. This function is created for
+##'   convenience and also so that the width of wrapping can be set by simply
+##'   changing the default here. Note that this give a leading white space to
+##'   each resulting line. The is done to account for the fact that wrapping
+##'   deletes the whites paces, and it actually looks good.
+##' @param x A single character string
+##' @param width The desired width
+##' @param ... Further arguments passed to 'strwrap()'
+##' @return a character string with returns placed such to achieve the desired
+##'   width
+##' @author Gilles Dutilh
+PrintWrap = function(x,
+                     width = 0.75 * getOption("width"), ...){
+  if (x == ''){
+    x
+  } else {
+    paste0(' ', paste0(strwrap(x, width = width), collapse = '\n '))
+  }
+}
