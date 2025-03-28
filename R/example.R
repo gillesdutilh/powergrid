8bb77a4f3bfecd3a97111fac518b3bea65dfbd6b
Gilles Dutilh <gilles.dutilh@gmail.com>
2025-03-28 15:52:11 +0100

    completed example

--- R/example.R
+++ R/example.R
@@ -1,72 +1,82 @@
-##' Find combination of parameters yielding desired power (or other value) in an
-##' object of class `power_array`.
+##' Find combination of parameters yielding desired power (or any other target
+##' value) in an object of class "power_array".
 ##'
-##' In the most common use case, and this is also the default, `Example`
+##' In the most typical use case, and this is also the default, `Example`
 ##' searches the *minimal* n where the power is *at least* equal to the value
-##' given by argument `target`. More generically, the object x, sliced at
-##' the values given in argument `example` results in a vector. This vector
-##' is searched for the *minimal* value of the names of the vector where the
-##' value of the vector is *at least* equal to `target`. Thus, when the
-##' example is a vector of power at different effect sizes, it searches the
-##' minimal effect size at which the target power is achieved.
+##' given by argument `target`. The function is, however, designed much more
+##' generically. The explanation below may be less helpful than trying the
+##' examples, but for completeness:
 ##'
-##' Above, *minimal* is defined by the default value TRUE of argument
-##' `find_min`. If `find_min` is FALSE, the maximum is searched. This
-##' is useful in the situation where one searches for the highest standard
-##' deviation at which it is still possible to find a desirable power.
+##' Argument `example` slices out a vector from object `x`, representing the
+##' values at the parameter combination given in example, thus, along the
+##' remaining parameter. Then, `Example` searches along this vector for the
+##' *minimal* parameter value where the value of the vector is *at least* equal
+##' to `target`. Thus, if the sliced out vector contains values of "power" along
+##' the parameter "effect size", it searches the minimal effect size at which
+##' the target power is achieved.
 ##'
-##' Above, *at least* is defined by the default value TRUE
-##' argument `minimal_target`. Setting `minimal_target` to FALSE allows
-##' to search, for example, for the minimal sample size where the expected
-##' confidence interval is smaller than a certain desired width.
+##' Two complications are made to allow for complete flexibility:
+##'
+##' 1) In the above description, *minimal* can be changed to *maximal* by setting argument
+##' `find_min` to FALSE. This is useful in the situation where one, e.g.,
+##' searches for the highest standard deviation at which it is still possible to
+##' find a desirable power.
+##'
+##' 2) In the above description, *at least* can be changed to *at most* by
+##' setting `minimal_target` to FALSE. This allows to search, for example, for
+##' the minimal sample size where the expected confidence interval is smaller
+##' than a certain desired width.
 ##'
 ##' @title Find Combination of Parameters Yielding Desired Value (typically
 ##'   power).
 ##' @param x Object of class `power_array` or `power`
-##' @param example List with named elements representing the parameter values at
-##'   which the example should be based. The names of this list should match the
-##'   dimension names of `x`, their values should be exact values at these
-##'   dimensions. Note that these are not indices, but values. See example for
-##'   an illustration.
+##' @param example List with named elements representing the constellation of
+##'   parameter values for which the example should be found. The names of this
+##'   list should match the dimension names of `x`, their values should be exact
+##'   values available at these dimensions. See example for an illustration.
 ##' @param target Which value (of typically power) should be achieved at the
 ##'   example.
-##' @param minimal_target Logical. Should target be minimally achieved (e.g.,
-##'   power), or maximially allowed (e.g., estimation uncertainty)?
+##' @param minimal_target Logical. Should target be minimally achieved (e.g., a
+##'   power of at least a certain value), or maximially allowed (e.g., a maximum
+##'   expected estimation uncertainty)?
 ##' @param find_min Logical, indicating whether the example should be found that
-##'   minimizes an assumption (e.g., minimal required n) to achieve the `target`
-##'   or maximizes this assumption (e.g., maximally allows SD).
+##'   minimizes a parameter (typically: minimal required n) to achieve the
+##'   `target` or maximizes this assumption (e.g., maximal allowed SD).
 ##' @param method Character string, indicating how the location of the example
-##'   is found, as implemented in `FindTarget`. Either `"step"`: walking in
-##'   steps along `search_par` or `"lm"`: Interpolating assuming a linear
-##'   relation between `search_par` and (qnorm(x) + qnorm(1 - 0.05)) ^
-##'   2. Setting `"lm"` is inspired on the implementation in the sse package by
-##'   Thomas Fabbro.
+##'   is found, passed on internally to `FindTarget`. Either "step": walking in
+##'   steps along `search_par` or "lm": Interpolating assuming a linear relation
+##'   between `search_par` and (qnorm(x) + qnorm(1 - 0.05)) ^ 2. This method
+##'   "lm" is inspired on the implementation in the sse package by Thomas
+##'   Fabbro.
 ##' @param summary_function When x' attribute `summarized` is FALSE, x is
-##'   summarized across sims using this function.
+##'   summarized across sims using this function before searching the example.
 ##' @return Returns a list containing:
 ##'
-##' - requested_example: the parameters at which the power (or whatever the
+##' - "requested_example": the parameters at which the power (or whatever the
 ##'   values represent) was searched to achieve level `target` (typically
 ##'   the minimal power, e.g., .9), searching along parameter `required
 ##'   name` (typically n).
 ##'
-##' - objective: was `required_name` searched to find the "min" or "max" of
+##' - "objective": was `required_name` searched to find the "min" or "max" of
 ##'   x.
 ##'
-##' - target: which value should the power (or any other value) have.
+##' - "target": which value should the power (or any other value) have.
 ##'
-##' - required_name: the parameter searched along to find the minimum (or
+##' - "required_name": the parameter searched along to find the minimum (or
 ##'   maximized if slot `searched` = 'max') to achieve objective. (typically n)
 ##'
-##' - required_value: the minimum (or maximum if `searched` = "max") for
+##' - "required_value": the minimum (or maximum if `searched` = "max") for
 ##'   parameter `required_name` (which is typically n)
 ##'
-##' - searched: was the "min" or "max" for `required_name` searched?
+##' - "searched": was the "min" or "max" for `required_name` searched?
 ##'
-##' - minimal_target: Is the target a minimum (TRUE, as typical for power) or a
+##' - "minimal_target": Is the target a minimum (TRUE, as typical for power) or a
 ##'   maximum (FALSE, e.g., an expected uncertainty level)
 ##' @author Gilles Dutilh
 ##' @examples
+##' ## ============================================
+##' ## Typical use case: find lowest n for power
+##' ## ============================================
 ##' sse_pars = list(
 ##'   n = seq(from = 10, to = 60, by = 2),
 ##'   delta = seq(from = 0.5, to = 1.5, by = 0.1), ## effect size
@@ -77,19 +87,69 @@
 ##'   return(ptt$power)
 ##' }
 ##' power_array = PowerGrid(pars = sse_pars, fun = PowFun, n_iter = NA)
-##'
+##' ##'
 ##' ex_out = Example(power_array,
 ##'                  example = list(delta = .7, sd = .7),
 ##'                  target = .9)
-##' ex_out
+##' ex_out #
 ##'
-##' ## Concerning argument `example`: The values in `example` refer to values of
-##' #the # parameters, and are *not indices*. This means that below, `delta = 1`
-##' #refers to the # *value* 1.0, not to the first value in the vector delta.
+##' ## ============================================
+##' ## Illustration argument `find_min`
+##' ## ============================================
+##' ##
+##' ## In this example, we search for the *highest sd* for which the power is at
+##' ## least .9.
 ##' ex_out = Example(power_array,
-##'                  example = list(delta = 1.0, sd = .7),
-##'                  target = .9)
-##' ex_out
+##'                  example = list(n = 40, delta = .7),
+##'                  target = .9, find_min = FALSE)
+##' ex_out # note how the printed result indicates it searched for a maximal
+##'                                         # permissible sd.
+##'
+##' ## ============================================
+##' ## Illustration argument `minimal_target`
+##' ## ============================================
+##' ##
+##' ## In the example below, we search for the lowest n where the expected CI-width
+##' ## is not larger than .88.
+##' PowFun <- function(n, delta, sd){
+##'   x1 = rnorm(n = n/2, sd = sd)
+##'   x2 = rnorm(n = n/2, mean = delta, sd = sd)
+##'   CI_width = diff(t.test(x1, x2)$conf.int) # CI95 is saved
+##' }
+##' sse_pars = list(
+##'   n = seq(from = 10, to = 60, by = 5),
+##'   delta = seq(from = 0.5, to = 1.5, by = 0.2),
+##'   sd = seq(.5, 1.5, .2))
+##' ## we iterate, and take the average across iterations to get expected CI-width:
+##' n_iter = 20
+##' set.seed(1)
+##' power_array = PowerGrid(pars = sse_pars, fun = PowFun, n_iter = n_iter)
+##' summary(power_array)
+##' ## Now, find lowest n for which the average CI width is *smaller than .88*.
+##' ex_out = Example(power_array,
+##'                  example = list(delta = .7, sd = .7),
+##'                  target = .88,
+##'                  find_min = TRUE, # we search the *lowest* n
+##'                  minimal_target = FALSE # for a *maximal* mean CI width
+##'                  )
+##' ex_out # note how the printed result indicates the target CI is a maximum.
+##'
+##' ## ============================================
+##' ## When both `find_min` and `minimal_target` are FALSE
+##' ## ============================================
+##' ##
+##' ## In this example, we search for the *highest sd* for which the average CI
+##' ## width is still *smaller than or equal to .88*.
+##' ex_out = Example(power_array,
+##'                  example = list(delta = .7, n = 60),
+##'                  target = .88,
+##'                  find_min = FALSE, # we search the *highest* sd
+##'                  minimal_target = FALSE # for a *maximal* mean CI width
+##'                  )
+##'
+##' ex_out # note how the printed result indicates that the *maximal permissible SD*
+##'        # was found for a CI of *at most .88*.
+##'
 Example = function(x,
                    example = NULL,
                    target = NULL,
