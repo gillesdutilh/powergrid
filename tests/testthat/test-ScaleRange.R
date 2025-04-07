89b2af579d2361a82f210596df95b88103c6aced
Alan Haynes <aghaynes@users.noreply.github.com>
2025-04-07 12:06:34 +0200

    add test example

---
+++ tests/testthat/test-ScaleRange.R
@@ -0,0 +1,17 @@
+
+
+x <- runif(100)
+scaled <- ScaleRange(x, 10, 100)
+
+test_that("ScaleRange minimum", {
+  expect_equal(min(scaled), 10)
+})
+test_that("ScaleRange maximum", {
+  expect_equal(max(scaled), 100)
+})
+test_that("ScaleRange length", {
+  expect_equal(length(scaled), 100)
+})
+test_that("ScaleRange same order", {
+  expect_true(all(order(scaled) == order(x)))
+})
