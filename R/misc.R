da39233c8c60cf3b6cb1bcf532b4e963645f8079
gillesdutilh <gilles.dutilh@gmail.com>
2025-09-02 14:12:03 +0200

    correct export

--- R/misc.R
+++ R/misc.R
@@ -1,12 +1,12 @@
 
 ##' @title Scale a vector to have a desired range
+##' @keywords internal
 ##' @description Linear scaling of a vector to a vector with a required range.
 ##' @param x A vector
 ##' @param a The desired minimum
 ##' @param b The desired maximum
 ##' @return a vector with the desired minimum and maximum
 ##' @author Gilles Dutilh
-##' @export
 ScaleRange = function(x, a, b){
 
   if(a > b) stop("a should be less than b")
@@ -15,6 +15,7 @@ ScaleRange = function(x, a, b){
 }
 
 ##' @title Wrap text in printed output.
+##' @keywords internal
 ##' @description Wrap text in printed output. This function is created for
 ##'   convenience and also so that the width of wrapping can be set by simply
 ##'   changing the default here. Note that this give a leading white space to
@@ -26,7 +27,6 @@ ScaleRange = function(x, a, b){
 ##' @return a character string with returns placed such to achieve the desired
 ##'   width
 ##' @author Gilles Dutilh
-##' @export
 PrintWrap = function(x,
                      ## width = 0.5 * (getOption("width") - 3), ...){
                      width = 48, ...){
@@ -38,6 +38,7 @@ PrintWrap = function(x,
 }
 
 ##' @title Draw a line of dashes, or something else with a certain width.
+##' @keywords internal
 ##' @description Draw a line of dashes, or something else with a certain
 ##'   width. Is used in package in combination with PrintWrap.
 ##' @param symbol Symbol to repeat `width` times.
