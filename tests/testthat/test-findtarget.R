882c2c2063fc5fa9123673a4766c18ddf9d483b8
gillesdutilh <gilles.dutilh@gmail.com>
2025-09-09 17:19:50 +0200

    all

--- tests/testthat/test-findtarget.R
+++ tests/testthat/test-findtarget.R
@@ -5,38 +5,38 @@ rm(list=ls())
 slice <- setNames(seq(0.09,0.89, 0.1), nm = seq(1,9))
 
 #' Minimum target above 0.5 (default) => 0.59
-result1 <- FindTarget(x = slice, target = 0.5)
+result1 <- FindTarget(x = slice, target_value = 0.5)
 test_that(
   "FindTarget (step) identifies correct value, under defaults",
   {expect_equal(result1, 6)}
 )
 
 #' Minimum target below 0.5 => 0.09
-result2 <- FindTarget(x = slice, target = 0.5,
-                      minimal_target = FALSE)
+result2 <- FindTarget(x = slice, target_value = 0.5,
+                      target_at_least = FALSE)
 test_that(
   "FindTarget (step) identifies correct value, target is a maximum",
   {expect_equal(result2, 1)}
 )
 
 #' Maximal target above 0.5 => 0.89
-result3 <- FindTarget(x = slice, target=0.5,
-                      find_min = FALSE)
+result3 <- FindTarget(x = slice, target_value=0.5,
+                      find_lowest = FALSE)
 test_that(
   "FindTarget (step) identifies correct value, minimal parameter value searched",
   {expect_equal(result3, 9)}
 )
 
 #' Maximal target below 0.5 => 0.49
-result4 <- FindTarget(x = slice, target = 0.5,
-                      minimal_target = FALSE, find_min = FALSE)
+result4 <- FindTarget(x = slice, target_value = 0.5,
+                      target_at_least = FALSE, find_lowest = FALSE)
 test_that(
   "FindTarget (step) identifies correct value, target is a maximum & minimal parameter value searched ",
   {expect_equal(result4, 5)}
 )
 ## ===============================================================
 
-#' The values of minimal_target and find_min are ignored if method
+#' The values of target_at_least and find_lowest are ignored if method
 #' equals "lm", thus tests are failed.
 
 set.seed(123)
@@ -44,7 +44,7 @@ slice <- setNames(seq(0.09,0.89, 0.1), nm = seq(0.1,0.9, 0.1)*10)
 slice <- slice + rnorm(n = length(slice), sd = 0.1)
 
 suppressWarnings(
-  result1 <- FindTarget(x = slice, target = 0.5, method = "lm")
+  result1 <- FindTarget(x = slice, target_value = 0.5, method = "lm")
 )
 test_that(
   "FindTarget (lm) identifies correct value, under defaults",
@@ -53,14 +53,14 @@ test_that(
 
 test_that(
   "FindTarget (lm) throws error if minimal target is FALSE",
-  {expect_error(FindTarget(x = slice, target = 0.5, method = "lm",
-                           minimal_target = FALSE))}
+  {expect_error(FindTarget(x = slice, target_value = 0.5, method = "lm",
+                           target_at_least = FALSE))}
 )
 
 test_that(
-  "FindTarget (lm) throws error if find_min is FALSE",
-  {expect_error(FindTarget(x = slice, target = 0.5, method = "lm",
-                           find_min = FALSE))}
+  "FindTarget (lm) throws error if find_lowest is FALSE",
+  {expect_error(FindTarget(x = slice, target_value = 0.5, method = "lm",
+                           find_lowest = FALSE))}
 )
 ## ===============================================================
 
@@ -72,7 +72,7 @@ test_that(
 #
 # test_that(
 #   "If the vector names are not numeric throw error",
-#   {expect_error(FindTarget(badname_slice, target=0.5))}
+#   {expect_error(FindTarget(badname_slice, target_value=0.5))}
 # )
 
 
