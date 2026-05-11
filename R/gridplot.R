c5d333166337ff3b23b2832703d0cb70022217da
gillesdutilh <gilles.dutilh@gmail.com>
2026-05-11 22:18:39 +0200

    added details grapchical parameters to gridplot

--- R/gridplot.R
+++ R/gridplot.R
@@ -23,6 +23,13 @@
 ##' to the grid (consider \code{Update}), and then adjusting the y-limit to only
 ##' include the values of interest.
 ##'
+##'   ### Graphical parameters
+##'   Graphical parameters that are allowed as input to the functions
+##'   `graphics::plot`, `graphics::lines`, `graphics::image` and `graphics::axis`,
+##'   including all parametes available in par() are passed on to these functions
+##'   internally. Not passed through are "x", "y", "z", "type", "at", and
+##'   parameters that are explict arguments to `PowerPlot`.
+##'
 ##' @param x An object of class "power_array" (from `powergrid`).
 ##' @param slicer If the parameter grid of `x` has more than 3 dimensions, a
 ##'   3-dimensional slice must be cut out using \code{slicer}, a list whose
