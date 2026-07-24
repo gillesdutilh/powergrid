092b901d7b90d7525212f23169691d297c8c6e35
gillesdutilh <gilles.dutilh@gmail.com>
2026-07-24 15:33:51 +0200

    changed if class(x) == y to is(x, y)

--- R/powergrid.R
+++ R/powergrid.R
@@ -773,19 +773,19 @@ SummarizeIterations = function(x, summary_function, ...){
   ## explicitly cover differnt situations, and define what the to be evaluated
   ## argument to apply below is. The latter is also the opject stored in
   ## attribute "summary_function".
-  if (class(summary_function) == 'name') { # function name inherited
+  if (is(summary_function, 'name')) { # function name inherited
     summary_function_name = as.character(summary_function)
     summary_function_eval = eval(summary_function)
-  } else if (class(summary_function) == 'call') { # ano function inherited
+  } else if (is(summary_function, 'call')) { # ano function inherited
     summary_function_name = 'anonymous function'
     summary_function_eval = eval(summary_function)
-  } else if (class(summary_function) == 'function' &
-             class(substitute(summary_function)) == 'name'
+  } else if (is(summary_function, 'function') &
+             is(substitute(summary_function), 'name')
              ) { # function name in direct SumarizeIterations use
     summary_function_name = as.character(substitute(summary_function))
     summary_function_eval = summary_function
-  } else if (class(summary_function) == 'function' &
-             class(substitute(summary_function)) == 'call'
+  } else if (is(summary_function, 'function') &
+             is(substitute(summary_function), 'call')
              ) { # function name in direct SumarizeIterations use
     summary_function_name = 'anonymous function'
     summary_function_eval = summary_function
